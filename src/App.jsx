8c2c351f7de059fc450e43721fbd86206e6c2055
Luka Cáceres <96329037+LukaCaceres@users.noreply.github.com>
2025-07-08 14:04:02 -0300

    about finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import FooterComponent from './components/FooterComponent'
 import NavbarComponent from './components/NavbarComponent'
+import About from './pages/About';
 import Home from './pages/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -15,6 +16,7 @@ function App() {
                 <main className='flex-grow'>
                     <Routes>
                         <Route path='/' element={<Home />} />
+                        <Route path='/nosotros' element={<About/>} />
                     </Routes>
 
                 </main>
